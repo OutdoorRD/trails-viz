e67376ebec1e9a7c749ea1684e46abc523c52ddc
davemfish <davemfish@gmail.com>
2018-02-28 15:38:41 -0800

    change tiles to stamen terrain

--- static/js/trail-map.js
+++ static/js/trail-map.js
@@ -8,15 +8,23 @@
 	function ready(error, geojsontrails){
 		var map = L.map('trail-map').setView([45, -105], 11); // lat/lon
 
-		L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+		// L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+		// 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+		// 	maxZoom: 18,
+		// 	id: 'osm',
+		// }).addTo(map);
+
+		L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}.{ext}', {
+			attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+			subdomains: 'abcd',
+			minZoom: 0,
 			maxZoom: 18,
-			id: 'osm',
+			ext: 'png'
 		}).addTo(map);
 
 		var trailstyle = {
 		    "color": "#ff7800",
-		    "weight": 1,
+		    "weight": 2,
 		    "opacity": 1.0
 		};
 
