e2a13282f482859aff6fcf3be211a4a137e3e648
davemfish <davemfish@gmail.com>
2018-03-26 10:04:06 -0700

    added basemap options. replaced terrain-background with terrain with labels. issue #3

--- static/js/trail-map.js
+++ static/js/trail-map.js
@@ -6,27 +6,30 @@
 		.await(ready);
 
 	function ready(error, geojsontrails){
-		var map = L.map('trail-map').setView([45, -105], 11); // lat/lon
+		// leaflet coordinate pairs are always [lat(y), lon(x)] as opposed to [x, y]
+		var map = L.map('trail-map').setView([45, -105], 11); 
 
-		// L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-		// 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-		// 	maxZoom: 18,
-		// 	id: 'osm',
-		// }).addTo(map);
+		var streets = L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+			maxZoom: 18,
+			id: 'osm',
+		});//.addTo(map);
 
-		L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}.{ext}', {
+		var terrain = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}', {
 			attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-			subdomains: 'abcd',
-			minZoom: 0,
 			maxZoom: 18,
 			ext: 'png'
 		}).addTo(map);
 
+		var baseMaps = {
+		    "terrain": terrain,
+		    "streets": streets
+		};
+
 		var getStyle = function(feature){
 		  var weight;
 		  weight = (feature.properties.annual*0.17)**4
-		  // console.log((feature.properties.annual*0.2)**2)
-		  console.log(weight);
+		  // console.log(weight);
 		  // weight = 2
 
 		  return {
@@ -47,6 +50,7 @@
 			style: getStyle
 		});
 
+		L.control.layers(baseMaps).addTo(map);
 		layer.addTo(map);
 		map.fitBounds(layer.getBounds());
 	}
