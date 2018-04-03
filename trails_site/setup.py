321956e632b2d1b41a78ee0cb04cbc3651181935
Stephen Moxley <smox98@uw.edu>
2018-04-03 15:10:40 -0700

    Configured the framework as a package and renamed to trails_site so the app can be imported in setup.py

---
+++ trails_site/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='trails_site',
+    packages=['trails_site'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
---
+++ trails_site/trails_site/__init__.py
@@ -0,0 +1,1 @@
+from .trails_site import app
