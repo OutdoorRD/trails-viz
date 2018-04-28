6680335b9f90f535b7469ea152a1c844c765c6c0
Stephen Moxley <smox98@uw.edu>
2018-04-28 13:55:50 -0700

    Finish adding ability to select two trails to graphs, add HTML and CSS to be able to show two trails' information at once

--- static/js/line-plot.js
+++ static/js/line-plot.js
@@ -83,26 +83,11 @@
 		var first = document.querySelector("ul li input");
 		first.dispatchEvent(startgraph);
 
-/*
-some inputs 1 and 2 keep the two possible input data variables
-
-the function to change the graph contains four inputs now -- d1, value1, d2, value2
-when a trail is unselected (i.e. going to one trail view), the input containing that
-data is cleared and the change graph function is called with d2 and value2 as null parameters
-when a trail is selected and selected==0, the function is called again w/ d2 and value2 as null parameters
-when a trail is selected and selected==1, the function is called again w/ all function inputs filled
-
-
-*/
-
-
 
 		function changeGraph(d1, value1, d2, value2) {
 
 			var trail_id_1 = value1,
 				filename1 = value1 + ".csv";
-
-
 
 			// Adds the annual averages statistic to the panel of text about the current trail
 			d3.csv("static/data/annuals/" + filename1)
@@ -115,7 +100,7 @@ when a trail is selected and selected==1, the function is called again w/ all fu
 					}
 					sum = Math.round(sum / rows.length);
 					console.log(sum);
-					var annual_avg = document.getElementById('total-annual-visits');
+					var annual_avg = document.querySelector('#statistics-info1 #total-annual-visits');
 					annual_avg.innerText = "Average Annual Visits: " + sum;
 				});
 
@@ -127,6 +112,8 @@ when a trail is selected and selected==1, the function is called again w/ all fu
 
 			var name1 = traildata1[0].Trail_name;
 			document.getElementById('trail-name').innerText = name;
+
+			document.querySelector("#statistics-info1 #trail-name").innerText = name1;
 
 			var date = [],
 				predicted1 = [],
@@ -141,7 +128,7 @@ when a trail is selected and selected==1, the function is called again w/ all fu
 			console.log(date);
 
 			predicted1.unshift(traildata1[0].Trail_name + ' - modeled');
-			actual1.unshift(traildata1[0].Trail_name + ' - actual');
+			actual1.unshift(traildata1[0].Trail_name + ' - on-site');
 			date.unshift('date');
 
 			var plot_data = [];
@@ -154,6 +141,23 @@ when a trail is selected and selected==1, the function is called again w/ all fu
 				var traildata2 = hikers_timeseries.filter(function(d) {
 					return (d.AllTRLs_ID == trail_id_2);
 				})
+
+				var name2 = traildata2[0].Trail_name;
+				document.querySelector("#statistics-info2 #trail-name").innerText = name2;
+
+				d3.csv("static/data/annuals/" + filename2)
+					.row(function(a) { return [a.year, a.total_pred]; })
+					.get(function(error, rows) {
+						var sum = 0;
+						for (let i = 0; i < rows.length; i++) {
+							var row = rows[i];
+							sum += parseInt(row[1]);
+						}
+						sum = Math.round(sum / rows.length);
+						console.log(sum);
+						var annual_avg = document.querySelector('#statistics-info2 #total-annual-visits');
+						annual_avg.innerText = "Average Annual Visits: " + sum;
+					});
 
 				var predicted2 = [],
 					actual2 = [];
@@ -171,6 +175,9 @@ when a trail is selected and selected==1, the function is called again w/ all fu
 				fillMonthly(filename1, traildata1[0].Trail_name, filename2, traildata2[0].Trail_name);
 
 			} else {
+				document.querySelector("#statistics-info2 #trail-name").innerText = "";
+				document.querySelector("#statistics-info2 #total-annual-visits").innerText = "";
+				document.querySelector("#statistics-info2 #total-summer-visits").innerText = "";
 				plot_data.push(date, predicted1, actual1);
 				fillMonthly(filename1, traildata1[0].Trail_name, null, null);
 			}
@@ -253,106 +260,6 @@ when a trail is selected and selected==1, the function is called again w/ all fu
 		document.querySelector("#trail-select select").dispatchEvent(changeEvent);
 }
 
-	function fillLine(plot_data) {
-	  var selected = false;
-
-	  var chart = c3.generate({
-	    bindto: '#line-plot',
-	    data: {
-	        x: 'date',
-	        xFormat: '%Y-%m', // 'xFormat' can be used as custom format of 'x'
-	        columns: plot_data
-	    },
-	    axis: {
-	        x: {
-	            type: 'timeseries',
-	            tick: {
-	              // culling: {max: 6},
-	                format: '%Y-%m',
-	                fit: false
-	            }
-	        },
-	        y: {
-	          label: {
-	            text: 'Number of Visits',
-	            position: 'outer-middle'
-	          }
-	        }
-	    },
-	     zoom: {
-	        enabled: true
-	    },
-	    legend: {
-	      item: {
-	        onclick: function(d) {
-	          if (selected) {
-	            // chart.unselect(d);
-	            // chart.defocus(d);
-	            chart.revert();
-	            selected = false;
-	          } else {
-	            chart.select(d);
-	            chart.focus(d);
-	            selected = true;
-	          }
-	        },
-	        onmouseout: function(d) {
-	          // prevent deselect on mouseout
-	        },
-	        onmouseover: function(d) {
-
-	        }
-	      }
-	    }
-	  });
-	}
-
-	// function fillMonthly(filename) {
-	// 	d3.csv("static/data/monthlies/" + filename)
-	// 		.row(function(a) {
-	// 			console.log(a);
-	// 			return [a.avg_pred]; }) // [a.month, a.avg_pred]; })
-	// 		.get(function(error, rows) {
-	// 			console.log(rows);
-	// 			rows.unshift (["Average Modeled"]);// (["Month", "Average Modeled"]);
-	//
-	// 			// Adds the summary about average summer visits to the panel with text about
-	// 			// the current trail
-	// 			var sum = parseInt(rows[6]) + parseInt(rows[7]) + parseInt(rows[8]);
-	// 			var summer = document.getElementById("total-summer-visits");
-	// 			summer.innerText = "Average Summer Visits: " + sum;
-	//
-	// 			var bar = c3.generate({
-	// 					bindto: '#histplot-monthlies-annuals',
-	// 					data: {
-	// 						rows: rows,
-	// 						// x: 'Month',
-	// 						type: 'bar'
-	// 					},
-	// 					axis: {
-	// 						x: {
-	// 							type: 'category',
-	// 							categories: ['Jan', 'Feb', 'March', 'April', 'May', 'June',
-	// 														'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-	// 							tick: {
-	// 								rotate: 60,
-	// 								multiline: false
-	// 							},
-	// 							height: 40
-	// 						},
-	// 						y: {
-	// 							label: {
-	// 								text: 'Average Modeled Number of Visits',
-	// 								position: 'outer-middle'
-	// 							}
-	// 						}
-	// 					},
-	// 					legend: {
-	// 						show: false
-	// 					}
-	// 			});
-	// 		});
-	// }
 
 	function fillMonthly(filename1, name1, filename2, name2) {
 	  var bar_data = [];
@@ -366,31 +273,31 @@ when a trail is selected and selected==1, the function is called again w/ all fu
 				for (var i = 0; i < rows.length; i++) {
 					bar_data[0].push(rows[i][0]);
 				}
+
+				var sum1 = parseInt(rows[6]) + parseInt(rows[7]) + parseInt(rows[8]);
+				var summer1 = document.querySelector("#statistics-info1 #total-summer-visits");
+				summer1.innerText = "Average Summer Visits: " + sum1;
+
 				if (filename2 == null) {
 					createMonthly(bar_data);
-				}
+				} else {
+					d3.csv("static/data/monthlies/" + filename2)
+						.row(function(a) { return [a.avg_pred]; })
+						.get(function(error, rows) {
+							rows.unshift([name2 + " - Average Modeled"]);
+							bar_data[1] = [];
+							for (var i = 0; i < rows.length; i++) {
+								bar_data[1].push(rows[i][0]);
+							}
 
-	      // insert sum data here for trail1 stats info
-	      /*
-	      var sum = parseInt(rows[6]) + parseInt(rows[7]) + parseInt(rows[8]);
-	      var summer = document.getElementById("total-summer-visits");
-	      summer.innerText = "Average Summer Visits: " + sum;
-	      */
+							var sum2 = parseInt(rows[6]) + parseInt(rows[7]) + parseInt(rows[8]);
+							var summer2 = document.querySelector("#statistics-info2 #total-summer-visits");
+							summer2.innerText = "Average Summer Visits: " + sum2;
+
+							createMonthly(bar_data);
+						});
+				}
 	    });
-		if(filename2 !== null) {
-		  d3.csv("static/data/monthlies/" + filename2)
-		    .row(function(a) { return [a.avg_pred]; })
-		    .get(function(error, rows) {
-		      rows.unshift([name2 + " - Average Modeled"]);
-					bar_data[1] = [];
-					console.log(rows);
-		      for (var i = 0; i < rows.length; i++) {
-						bar_data[1].push(rows[i][0]);
-					}
-					createMonthly(bar_data);
-			});
-		console.log(bar_data);
-		}
 	}
 
 	function createMonthly(bar_data) {
@@ -425,38 +332,6 @@ when a trail is selected and selected==1, the function is called again w/ all fu
 	}
 
 	function fillAnnual(filename1, name1, filename2, name2) {
-		// d3.csv("static/data/annuals/" + filename)
-		// 	.row(function(a) { return [a.year, a.total_pred]; })
-		// 	.get(function(error, rows) {
-		// 		rows.unshift (["Year", "Total Annual"]);
-		// 		console.log(rows);
-		// 		var bar = c3.generate({
-		// 				bindto: '#histplot-monthlies-annuals',
-		// 				data: {
-		// 					rows: rows,
-		// 					x: 'Year',
-		// 					type: 'bar'
-		// 				},
-		// 				axis: {
-		// 					x: {
-		// 						tick: {
-		// 							rotate: 60,
-		// 							multiline: false
-		// 						},
-		// 						height: 40
-		// 					},
-		// 					y: {
-		// 						label: {
-		// 							text: 'Modeled Number of Visits',
-		// 							position: 'outer-middle'
-		// 						}
-		// 					}
-		// 				},
-		// 				legend: {
-		// 					show: false
-		// 				}
-		// 		});
-		// 	});
 		var bar_data = [];
 
 	  d3.csv("static/data/annuals/" + filename1)
@@ -484,19 +359,6 @@ when a trail is selected and selected==1, the function is called again w/ all fu
 			      });
 				}
 	    });
-
-	  // if(filename2 != null) {
-	  //   d3.csv("static/data/annuals/" + filename2)
-	  //     .row(function(a) {return [a.total_pred]; })
-	  //     .get(function(error, rows) {
-	  //       rows.unshift(["Year", name2 + " - Total Annual"]);
-	  //       bar_data[2] = [];
-	  //       for (var i = 0; i < rows.length; i++) {
-	  //         bar_data[2].push(rows[i][0]);
-	  //       }
-	  //       createAnnual(bar_data);
-	  //     });
-	  // }
 	}
 
 	function createAnnual(bar_data) {
