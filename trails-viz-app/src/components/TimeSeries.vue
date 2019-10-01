6f36ce0935064c84af1a9f6c488a2bc3b741a572
Vivek Kumar <vivekuma@uw.edu>
2019-10-01 04:33:11 -0700

    use weekly visitation in time series when zoomed in issue #26

--- trails-viz-app/src/components/TimeSeries.vue
+++ trails-viz-app/src/components/TimeSeries.vue
@@ -22,42 +22,77 @@
         siteid: null,
         trailName: null,
         selectedSite: null,
-        monthlyVisitation: null
+        monthlyVisitation: null,
+        dataRange: ''
       }
     },
     methods: {
+      _getNthSunday: function(year, n) {
+        let date = new Date(year, 0);
+        let firstSunday;
+        if (date.getDay() === 0) {
+          firstSunday = date.getDate();
+        } else {
+          firstSunday = date.getDate() + 7 - date.getDay()
+        }
+        let dayOfYear = (n - 1) * 7 + firstSunday;
+        date.setDate(dayOfYear);
+        return date.getDate();
+      },
       renderTimeSeries: function () {
         let self = this;
         this.selectedSite = store.selectedSite;
         this.trailName = store.selectedSite['trailName'];
         this.siteid = store.selectedSite['siteid'];
         this.monthlyVisitation = store.monthlyVisitation;
+        this.weeklyVisitation = store.weeklyVisitation;
 
-        let dates = ['date'];
-        let modelled = ['Modelled'];
-        let onsite = ['On Site'];
-        let flickr = ['Flickr'];
-        let instag = ['Instagram'];
-        let twitter = ['Twitter'];
-        let wta = ['WTA'];
+        let monthlyDates = ['date'];
+        let monthlyModelled = ['Modelled'];
+        let monthlyOnsite = ['On Site'];
+        let monthlyFlickr = ['Flickr'];
+        let monthlyInstag = ['Instagram'];
+        let monthlyTwitter = ['Twitter'];
+        let monthlyWta = ['WTA'];
 
         self.monthlyVisitation.forEach(x => {
-          dates.push(x.year + '-' + x.month);
-          modelled.push(Math.round(x.estimate, 2));
-          onsite.push(Math.round(x.onsite, 2));
-          flickr.push(x.flickr);
-          instag.push(x.instag);
-          twitter.push(x.twitter);
-          wta.push(x.wta);
+          monthlyDates.push(x.year + '-' + x.month + '-1');
+          monthlyModelled.push(Math.round(x.estimate, 2));
+          monthlyOnsite.push(Math.round(x.onsite, 2));
+          monthlyFlickr.push(x.flickr);
+          monthlyInstag.push(x.instag);
+          monthlyTwitter.push(x.twitter);
+          monthlyWta.push(x.wta);
         });
-        let timeseriesData = [dates, modelled, onsite, flickr, instag, twitter, wta];
+        let timeseriesMonthlyData = [monthlyDates, monthlyModelled, monthlyOnsite, monthlyFlickr, monthlyInstag, monthlyTwitter, monthlyWta];
 
-        c3.generate({
+        let weeklyDates = ['date'];
+        let weeklyModelled = ['Modelled'];
+        let weeklyOnsite = ['On Site'];
+        let weeklyFlickr = ['Flickr'];
+        let weeklyInstag = ['Instagram'];
+        let weeklyTwitter = ['Twitter'];
+        let weeklyWta = ['WTA'];
+
+        self.weeklyVisitation.forEach(x => {
+          let sunday = self._getNthSunday(x.year, x.week);
+          weeklyDates.push(x.year + '-' + x.month + '-' + sunday);
+          weeklyModelled.push(Math.round(x.estimate, 2));
+          weeklyOnsite.push(Math.round(x.onsite, 2));
+          weeklyFlickr.push(x.flickr);
+          weeklyInstag.push(x.instag);
+          weeklyTwitter.push(x.twitter);
+          weeklyWta.push(x.wta);
+        });
+        let timeseriesWeeklyData = [weeklyDates, weeklyModelled, weeklyOnsite, weeklyFlickr, weeklyInstag, weeklyTwitter, weeklyWta];
+
+        self.dataRange = 'monthly';
+        let chart = c3.generate({
           bindto: '#time-series',
           data: {
             x: 'date',
-            xFormat: '%Y-%m', // 'xFormat' can be used as custom format of 'x'
-            columns: timeseriesData
+            xFormat: '%Y-%m-%d', // 'xFormat' can be used as custom format of 'x'
+            columns: timeseriesMonthlyData
           },
           axis: {
             x: {
@@ -75,7 +110,27 @@
             }
           },
           zoom: {
-            enabled: true
+            enabled: true,
+            rescale: true,
+            onzoom: function (domain) {
+              let startDate = domain[0];
+              let endDate = domain[1];
+              let diffTime = Math.abs(endDate - startDate);
+              let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+              if (self.dataRange === 'monthly' && diffDays <= 500) {
+                self.dataRange = 'weekly';
+                chart.load({
+                  columns: timeseriesWeeklyData
+                });
+                chart.zoom(domain);
+              } else if (self.dataRange === 'weekly' && diffDays > 500) {
+                self.dataRange = 'monthly';
+                chart.load({
+                  columns: timeseriesMonthlyData
+                });
+                chart.zoom(domain);
+              }
+            }
           }
         });
       }
