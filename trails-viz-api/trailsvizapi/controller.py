fa53e166507ff8bfa90780b0901af0d2c267b1c4
Vivek Kumar <vivekuma@uw.edu>
2019-10-14 22:59:39 -0700

    add /api in endpoints again

--- trails-viz-api/trailsvizapi/controller.py
+++ trails-viz-api/trailsvizapi/controller.py
@@ -3,37 +3,37 @@ from flask import request, Response, jsonify
 from trailsvizapi import app, data_repository, app_config
 
 
-@app.route('/projects')
+@app.route('/api/projects')
 def get_all_projects():
     return jsonify(app_config.PROJECT_GROUPS)
 
 
-@app.route('/sites/geojson')
+@app.route('/api/sites/geojson')
 def get_geojson_data():
     project_group = request.args.get('projectGroup')
     data = data_repository.get_project_sites(project_group)
     return Response(data.to_json(), mimetype='application/json')
 
 
-@app.route('/sites/<int:siteid>/monthlyEstimates')
+@app.route('/api/sites/<int:siteid>/monthlyEstimates')
 def get_monthly_estimates(siteid):
     data = data_repository.get_monthly_estimates(siteid)
     return Response(data.to_json(orient='records'), mimetype='application/json')
 
 
-@app.route('/sites/<int:siteid>/annualEstimates')
+@app.route('/api/sites/<int:siteid>/annualEstimates')
 def get_annual_estimates(siteid):
     data = data_repository.get_annual_estimates(siteid)
     return Response(data.to_json(orient='records'), mimetype='application/json')
 
 
-@app.route('/sites/<int:siteid>/monthlyVisitation')
+@app.route('/api/sites/<int:siteid>/monthlyVisitation')
 def get_monthly_visitation(siteid):
     data = data_repository.get_monthly_visitation(siteid)
     return Response(data.to_json(orient='records'), mimetype='application/json')
 
 
-@app.route('/sites/<int:siteid>/weeklyVisitation')
+@app.route('/api/sites/<int:siteid>/weeklyVisitation')
 def get_weekly_visitation(siteid):
     data = data_repository.get_weekly_visitation(siteid)
     return Response(data.to_json(orient='records'), mimetype='application/json')
