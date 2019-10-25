6018b1267aa8671a86d587f506b215e6e48b49af
Vivek Kumar <vivekuma@uw.edu>
2019-10-25 03:50:54 -0700

    update home locations API to return in tree data structure

--- trails-viz-api/trailsvizapi/controller.py
+++ trails-viz-api/trailsvizapi/controller.py
@@ -42,4 +42,4 @@ def get_weekly_visitation(siteid):
 @app.route('/api/sites/<int:siteid>/homeLocations')
 def get_home_locations(siteid):
     data = data_repository.get_home_locations(siteid)
-    return Response(data.to_json(orient='records'), mimetype='application/json')
+    return jsonify(data)
--- trails-viz-api/trailsvizapi/data_repository.py
+++ trails-viz-api/trailsvizapi/data_repository.py
@@ -134,4 +134,31 @@ def get_annual_estimates(siteid):
 def get_home_locations(siteid):
     home_locations = _get_from_data_source('HOME_LOCATIONS_DF')
     site_home_locations = home_locations[home_locations['siteid'] == siteid]
-    return site_home_locations
+    visit_days = site_home_locations['visit_days'].sum()
+    visitors_unq = site_home_locations['visitors_unq'].sum()
+    tree = dict()
+
+    # JSON doesn't recognize any numpy data types, hence must be converted to int
+    tree['siteid'] = int(site_home_locations.iloc[0, 0])
+    tree['visit_days'] = int(visit_days)
+    tree['visitors_unq'] = int(visitors_unq)
+    tree['countries'] = []
+
+    for country_idx, country_row in site_home_locations.groupby(by=['country']).sum().iterrows():
+        country_data = {'name': country_idx, 'visit_days': int(country_row['visit_days']),
+                        'visitors_unq': int(country_row['visitors_unq']),  'states': []}
+        tree['countries'].append(country_data)
+        country_df = site_home_locations[site_home_locations['country'] == country_idx]
+
+        for state_idx, state_row in country_df.groupby(by=['state']).sum().iterrows():
+            state_data = {'name': state_idx, 'visit_days': int(state_row['visit_days']),
+                          'visitors_unq': int(state_row['visitors_unq']), 'counties': []}
+            country_data['states'].append(state_data)
+            state_df = country_df[country_df['state'] == state_idx]
+
+            for county_idx, county_row in state_df.groupby(by=['county']).sum().iterrows():
+                county_data = {'name': county_idx, 'visit_days': int(county_row['visit_days']),
+                               'visitors_unq': int(county_row['visitors_unq'])}
+                state_data['counties'].append(county_data)
+
+    return tree
