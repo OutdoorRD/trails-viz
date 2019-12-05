f98c098ffe208b9554c7169f6b4491728f70f026
Vivek Kumar <vivekuma@uw.edu>
2019-12-05 14:44:06 -0800

    Front end changes to get visitation info for each project #63

--- trails-viz-app/src/components/InfoViewer.vue
+++ trails-viz-app/src/components/InfoViewer.vue
@@ -19,11 +19,11 @@
       renderInfo: function (type) {
         let self = this;
         let url;
+        let projectCode = self.$store.getters.getSelectedProjectCode;
         if (type === 'project') {
-          let projectCode = self.$store.getters.getSelectedProjectCode;
-          url = self.$apiEndpoint + '/projects/' + projectCode + '/readme'
+          url = self.$apiEndpoint + '/projects/' + projectCode + '/readme?type=INFO'
         } else if (type === 'visitation') {
-          url = self.$apiEndpoint + '/readme?type=VISITATION_INFO'
+          url = self.$apiEndpoint + '/projects/' + projectCode + '/readme?type=VISITS'
         } else if (type === 'homeLocations') {
           url = self.$apiEndpoint + '/readme?type=HOMELOCATIONS_INFO'
         }
