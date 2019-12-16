c7928770d108faf67c70f8042ad940807b756a45
Vivek Kumar <vivekuma@uw.edu>
2019-12-16 01:06:26 -0800

    remove transition package and from router #57

--- trails-viz-app/src/router.js
+++ trails-viz-app/src/router.js
@@ -1,23 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import VuePageTransition from 'vue-page-transition'
 import LandingPage from './components/LandingPage'
 import Dashboard from './components/Dashboard'
 
 Vue.use(VueRouter);
-Vue.use(VuePageTransition);
 
 const routes = [
   {
     path: '/',
-    component: LandingPage,
-    meta: {transition: 'fade'}
+    component: LandingPage
   },
   {
     path: '/dashboard/:project',
     name: 'dashboard',
-    component: Dashboard,
-    meta: {transition: 'fade'}
+    component: Dashboard
   }
 ];
 
