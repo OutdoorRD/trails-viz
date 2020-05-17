bfd5bad4af664a075448071f8d6a6e011d48d7ce
Spencer Wood <woodsp@stanford.edu>
2020-05-17 11:16:08 -0700

    Add OKW NVUM project with GFA and WILD sub-projects

--- trails-viz-api/trailsvizapi/config/app_config.py
+++ trails-viz-api/trailsvizapi/config/app_config.py
@@ -9,6 +9,8 @@ AUTH_BUCKET_NAME = 'recsetgo'
 PROJECT_NAMES = {
     'Western Washington': 'MBS_PIL',
     'Middle Fork': 'MBS_SARL',
+    'Okanogan-Wenatchee Wilderness': 'OKW_WILD',
+    'Okanogan-Wenatchee General Forest': 'OKW_GFA',
     'Northern New Mexico': 'DOI_PIL',
     'King County Parks': 'KingCo'
 }
@@ -16,6 +18,8 @@ PROJECT_NAMES = {
 CENSUS_TRACT_STATES = {
     'MBS_PIL': ['53'],
     'MBS_SARL': ['53'],
+    'OKW_WILD': ['53'],
+    'OKW_GFA': ['53'],
     'MBS_PIL, MBS_SARL': ['53'],
     'DOI_PIL': ['35'],
     'KingCo': ['53']
@@ -24,6 +28,8 @@ CENSUS_TRACT_STATES = {
 DATA_COLUMNS = {
     'MBS_PIL': ['estimate', 'flickr', 'twitter', 'instag', 'wta', 'onsite'],
     'MBS_SARL': ['estimate', 'flickr', 'twitter', 'instag', 'wta', 'onsite'],
+    'OKW_WILD': ['flickr', 'twitter', 'instag', 'wta'],
+    'OKW_GFA': ['flickr', 'twitter', 'instag', 'wta'],
     'DOI_PIL': ['estimate', 'flickr', 'twitter', 'instag', 'onsite'],
     'KingCo': ['flickr', 'twitter', 'instag', 'wta']
 }
