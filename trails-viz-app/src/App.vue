b3b09a4e63ebcd0d6c559bb1341e47c7acfe3e1a
davidye007 <davidyeap@outlook.com>
2024-09-25 14:46:54 -0700

    delete dummy comments in app folder and api folder

--- trails-viz-app/src/App.vue
+++ trails-viz-app/src/App.vue
@@ -23,7 +23,6 @@ export default {
   },
   mounted() {
     let self = this;
-    // dummy comment to test CICD: "hello world"
     // add interceptor to show modal when 403 response is received
     axios.interceptors.response.use(
       response => response,
