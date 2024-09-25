da8e8976dbb95ec35e303fe1aef5dae6e8c53c7b
davidye007 <davidyeap@outlook.com>
2024-09-25 11:25:52 -0700

    removed dummy comment

--- trails-viz-app/src/App.vue
+++ trails-viz-app/src/App.vue
@@ -23,7 +23,6 @@ export default {
   },
   mounted() {
     let self = this;
-    // remove this comment
     // add interceptor to show modal when 403 response is received
     axios.interceptors.response.use(
       response => response,
