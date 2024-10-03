8cdf58337231833ecbb75946118f1c41d3334b19
davidye007 <davidyeap@outlook.com>
2024-10-02 17:03:46 -0700

    nginx and uwsgi configurations for digital ocean droplet vm

--- docker-conf/start.sh
+++ docker-conf/start.sh
@@ -1,3 +1,31 @@
 #!/bin/bash -e
-uwsgi /app/uwsgi-config.ini &
-nginx -g 'daemon off;'
+
+# Stop running nginx and uWSGI processes
+pkill nginx || echo "nginx not running"
+pkill uwsgi || echo "uWSGI not running"
+
+# Start uWSGI
+/home/nginx/miniconda3/envs/trails-viz-api/bin/uwsgi --ini /home/nginx/trails-viz/trails-viz-api/uwsgi-config.ini &
+
+# Capture the PID of the uWSGI process
+UWSGI_PID=$!
+
+# Start nginx
+nginx -c /home/nginx/trails-viz/docker-conf/nginx.conf -g 'daemon off;' &
+
+# Capture the PID of the nginx process
+NGINX_PID=$!
+
+# Function to handle termination signals
+cleanup() {
+    echo "Stopping services..."
+    kill -TERM "$UWSGI_PID" "$NGINX_PID"
+    wait "$UWSGI_PID" "$NGINX_PID"
+    echo "Services stopped."
+}
+
+# Trap termination signals
+trap cleanup SIGINT SIGTERM
+
+# Wait for the services to exit
+wait "$UWSGI_PID" "$NGINX_PID"
