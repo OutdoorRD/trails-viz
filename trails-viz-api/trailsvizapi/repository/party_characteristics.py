f7967ff0343fdd1fa8c86de6e1bc4cd3f1749e8e
davidye007 <davidyeap@outlook.com>
2024-11-18 12:57:10 -0800

    Added endpoints for visualizing party characteristic counts/observations.

--- trails-viz-api/trailsvizapi/repository/party_characteristics.py
+++ trails-viz-api/trailsvizapi/repository/party_characteristics.py
@@ -1,43 +1,25 @@
 import numpy as np
 import geopandas as gpd
+import pandas as pd
 from trailsvizapi.repository.prepare_data import get_from_data_source
 from trailsvizapi.repository.projects_and_sites import get_project_sites
 
 def _get_project_party_characteristics_data(project):
     project_sites = get_project_sites(project)
-    party_characteristics_df = get_from_data_source('PARTY_CHARACTERISTICS_DF')
     siteids = set(project_sites['siteid'].unique())
-    # Add a new column in df_vic_siteid called trail that contains the project site ids (None if observation is not part of project siteids)
-    party_characteristics_df.loc[:,'trail'] = party_characteristics_df['SiteID'].apply(lambda lst: next((x for x in lst if x in siteids), None))
-    party_characteristics_df = party_characteristics_df.dropna(subset=['trail'])
-    return party_characteristics_df
+    party_characteristics_df = get_from_data_source('PARTY_CHARACTERISTICS_DF')
+    # Add a 'trail' feature that contains the project site ids (None if observation is not part of project siteids)
+    party_characteristics_df['trail'] = party_characteristics_df['SiteID'].apply(lambda lst: next((x for x in lst if x in siteids), None))
+    # Return observations with non-missing 'trail' values
+    return party_characteristics_df.dropna(subset=['trail'])
 
 def _get_party_characteristics_data(siteid):
     party_characteristics_df = get_from_data_source('PARTY_CHARACTERISTICS_DF')
-    site_data = party_characteristics_df[party_characteristics_df['SiteID'].apply(lambda x: siteid in x)]
-    site_data['trail'] = siteid
-    return site_data
-
-def get_project_party_people(project):
-    return _get_project_party_characteristics_data(project)[['PartyPeople', 'trail']].dropna(subset=['PartyPeople'])
-
-def get_project_party_vehicles(project):
-    return _get_project_party_characteristics_data(project)[['PartyVehics', 'trail']].dropna(subset=['PartyVehics'])
-
-def get_project_trail_visits(project):
-    return _get_project_party_characteristics_data(project)[['TrailVisits', 'trail']].dropna(subset=['TrailVisits'])
+    party_characteristics_df['trail'] = party_characteristics_df['SideID'].apply(lambda x: siteid if siteid in x else None)
+    return party_characteristics_df.dropna(subset=['trail'])
 
 def get_project_party_characteristics(project):
     return _get_project_party_characteristics_data(project)[['TrailVisits', 'PartyPeople','PartyVehics', 'trail']]
-
-def get_party_people(siteid):
-    return _get_party_characteristics_data(siteid)[['PartyPeople', 'trail']].dropna(subset=['PartyPeople'])
-
-def get_party_vehicles(siteid):
-    return _get_party_characteristics_data(siteid)[['PartyVehics', 'trail']].dropna(subset=['PartyVehics'])
-
-def get_trail_visits(siteid):
-    return _get_party_characteristics_data(siteid)[['TrailVisits', 'trail']].dropna(subset=['TrailVisits'])
 
 def get_party_characteristics(siteid):
     return _get_party_characteristics_data(siteid)[['TrailVisits', 'PartyPeople','PartyVehics', 'trail']]
