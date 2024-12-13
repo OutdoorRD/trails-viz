b0cb25a4d09e32f8c74cc4ba45b05c9ef0dd0f22
davidye007 <davidyeap@outlook.com>
2024-12-12 23:12:18 -0800

    Update party characteristics endpoints, add endpoints for calculating yearly averages

--- trails-viz-api/trailsvizapi/controller/party_characteristics.py
+++ trails-viz-api/trailsvizapi/controller/party_characteristics.py
@@ -1,6 +1,7 @@
 from flask import Response
 from trailsvizapi import app
 from trailsvizapi.repository import party_characteristics
+import json
 
 
 @app.route('/api/projects/<string:project>/partyCharacteristics')
@@ -13,3 +14,15 @@ def get_project_party_characteristics(project):
 def get_party_characteristics(siteid):
     data = party_characteristics.get_party_characteristics(siteid)
     return Response(data.to_json(orient='records'), mimetype='application/json')
+
+
+@app.route('/api/projects/<string:project>/partyCharacteristicsYearlyStatistics')
+def get_project_party_characteristics_yearly_statistics(project):
+    data = party_characteristics.get_project_party_characteristics_yearly_statistics(project)
+    return Response(json.dumps(data), mimetype='application/json')
+
+
+@app.route('/api/sites/<string:siteid>/partyCharacteristicsYearlyStatistics')
+def get_party_characteristics_yearly_statistics(siteid):
+    data = party_characteristics.get_party_characteristics_yearly_statistics(siteid)
+    return Response(json.dumps(data), mimetype='application/json')
