5b5828aa8b0e08735811eb5dde521facdeafc06c
davidye007 <davidyeap@outlook.com>
2025-01-05 19:56:08 -0800

    data for a single feature returned per api request - a single api call no longer returns data for all features, but only for the specified feature (seperation of concerns)

--- trails-viz-api/trailsvizapi/controller/party_characteristics.py
+++ trails-viz-api/trailsvizapi/controller/party_characteristics.py
@@ -4,25 +4,22 @@ from trailsvizapi.repository import party_characteristics
 import json
 
 
-@app.route('/api/projects/<string:project>/partyCharacteristics')
-def get_project_party_characteristics(project):
-    data = party_characteristics.get_project_party_characteristics(project)
-    return Response(data.to_json(orient='records'), mimetype='application/json')
+
+@app.route('/api/projects/<string:project>/partyCharacteristics/<string:characteristic>')
+def get_project_party_characteristics(project, characteristic):
+    return party_characteristics.get_project_party_characteristics(project, characteristic)
 
 
-@app.route('/api/sites/<string:siteid>/partyCharacteristics')
-def get_party_characteristics(siteid):
-    data = party_characteristics.get_party_characteristics(siteid)
-    return Response(data.to_json(orient='records'), mimetype='application/json')
+@app.route('/api/sites/<string:siteid>/partyCharacteristics/<string:characteristic>')
+def get_party_characteristics(siteid, characteristic):
+    return party_characteristics.get_party_characteristics(siteid, characteristic)
 
 
-@app.route('/api/projects/<string:project>/partyCharacteristicsYearlyStatistics')
-def get_project_party_characteristics_yearly_statistics(project):
-    data = party_characteristics.get_project_party_characteristics_yearly_statistics(project)
-    return Response(json.dumps(data), mimetype='application/json')
+@app.route('/api/projects/<string:project>/partyCharacteristicsYearlyStatistics/<string:characteristic>')
+def get_project_party_characteristics_yearly_statistics(project, characteristic):
+    return party_characteristics.get_project_party_characteristics_yearly_statistics(project, characteristic)
 
 
-@app.route('/api/sites/<string:siteid>/partyCharacteristicsYearlyStatistics')
-def get_party_characteristics_yearly_statistics(siteid):
-    data = party_characteristics.get_party_characteristics_yearly_statistics(siteid)
-    return Response(json.dumps(data), mimetype='application/json')
+@app.route('/api/sites/<string:siteid>/partyCharacteristicsYearlyStatistics/<string:characteristic>')
+def get_party_characteristics_yearly_statistics(siteid, characteristic):
+    return party_characteristics.get_party_characteristics_yearly_statistics(siteid, characteristic)
--- trails-viz-api/trailsvizapi/repository/party_characteristics.py
+++ trails-viz-api/trailsvizapi/repository/party_characteristics.py
@@ -1,3 +1,5 @@
+import json
+from flask import Response, jsonify
 from trailsvizapi.repository.prepare_data import get_from_data_source
 from trailsvizapi.repository.projects_and_sites import get_project_sites
 import pandas as pd
@@ -5,111 +7,173 @@ import numpy as np
 import math
 
 CONFIDENCE_LEVEL = 1.96
-PARTY_CHARACTERISTICS = ['TrailVisits', 'PartyPeople', 'PartyVehics', 'PeoplePerVehics']
+# PARTY_CHARACTERISTICS = ['TrailVisits', 'PartyPeople', 'PartyVehics', 'PeoplePerVehics']
 
-
-def get_project_party_characteristics(project):
+def get_project_party_characteristics(project, characteristic):
     project_sites = get_project_sites(project)
     siteids = set(project_sites['siteid'].unique())
-    party_characteristics_df = _prepare_party_characteristics_wide_format(siteids=siteids)
-    if party_characteristics_df.empty:
-        return party_characteristics_df
-    return _convert_wide_to_long_format(party_characteristics_df).dropna(subset=['value'])
-
-
-def get_party_characteristics(siteid):
-    party_characteristics_df = _prepare_party_characteristics_wide_format(siteid=siteid)
-    if party_characteristics_df.empty:
-        return party_characteristics_df
-    return _convert_wide_to_long_format(party_characteristics_df).dropna(subset=['value'])
-
-
-def get_project_party_characteristics_yearly_statistics(project):
-    project_sites = get_project_sites(project)
-    siteids = set(project_sites['siteid'].unique())
-    party_characteristics_df = _prepare_party_characteristics_wide_format(siteids=siteids)
-    if party_characteristics_df.empty:
-        return _generate_empty_time_series_response()
-    party_characteristics_df = _convert_wide_to_long_format(party_characteristics_df)
-    return _calculate_yearly_mean_and_CI(party_characteristics_df)
-
-
-def get_party_characteristics_yearly_statistics(siteid):
-    party_characteristics_df = _prepare_party_characteristics_wide_format(siteid=siteid)
-    if party_characteristics_df.empty:
-        return _generate_empty_time_series_response()
-    party_characteristics_df = _convert_wide_to_long_format(party_characteristics_df)
-    return _calculate_yearly_mean_and_CI(party_characteristics_df)
-
-
-def _prepare_party_characteristics_wide_format(siteids=None, siteid=None):
     party_characteristics_df = get_from_data_source('PARTY_CHARACTERISTICS_DF')
-    # Add a 'trail' feature that contains the project site ids (None if observation is not part of project siteids)
-    if siteids:
-        party_characteristics_df['trail'] = party_characteristics_df['SiteID'].apply(
-            lambda lst: next((x for x in lst if x in siteids), None))
-    elif siteid:
-        party_characteristics_df['trail'] = party_characteristics_df['SiteID'].apply(
-            lambda x: siteid if siteid in x else None)
-    else:
-        raise ValueError("Either siteids or siteid must be provided")
+    # Add a 'trail' feature that contains the project assoicated site id.
+    # 'trail' value set to None if no site ids in 'SiteID' are not included in the list of project siteids.
+    party_characteristics_df['trail'] = party_characteristics_df['SiteID'].apply(
+        lambda lst: next((x for x in lst if x in siteids), None))
     # keep observations with non-missing 'trail' values
     party_characteristics_df = party_characteristics_df.dropna(subset=['trail'])
     if party_characteristics_df.empty:
-        return party_characteristics_df
-    return _add_additional_features(party_characteristics_df)
-
-
-def _convert_wide_to_long_format(df):
-    df_long = df.melt(
+        return Response(status=204)
+    if characteristic not in party_characteristics_df.columns:
+        party_characteristics_df = _add_additional_features(party_characteristics_df, characteristic)
+        if characteristic not in party_characteristics_df.columns:
+            return Response(status=204)
+    # add year feature
+    party_characteristics_df.loc[:, 'year'] = pd.to_datetime(party_characteristics_df['date']).dt.year
+    party_characteristics_df_long = party_characteristics_df.melt(
         id_vars=['date', 'year', 'trail'],
-        value_vars=PARTY_CHARACTERISTICS,
+        value_vars=[characteristic],
         var_name='characteristic',
         value_name='value'
     )
-    return df_long
+    party_characteristics_df_long = party_characteristics_df_long.dropna(subset=['value'])
+    if party_characteristics_df_long.empty:
+        return Response(status=204)
+    response_data = party_characteristics_df_long.to_json(orient='records')
+    return Response(response_data, status = 200, mimetype='application/json')
 
 
-def _add_additional_features(df):
-    df = _add_people_per_vehics_feature(df)
-    df = _add_year_feature(df)
-    return df
+def get_party_characteristics(siteid, characteristic):
+    party_characteristics_df = get_from_data_source('PARTY_CHARACTERISTICS_DF')
+    # Add a 'trail' feature that contains the given siteid.
+    # 'trail' value set to None if 'SiteID' does not include siteid.
+    party_characteristics_df['trail'] = party_characteristics_df['SiteID'].apply(
+        lambda x: siteid if siteid in x else None)
+    # keep observations with non-missing 'trail' values
+    party_characteristics_df = party_characteristics_df.dropna(subset=['trail'])
+    if party_characteristics_df.empty:
+        return Response(status=204)
+    if characteristic not in party_characteristics_df.columns:
+        party_characteristics_df = _add_additional_features(party_characteristics_df, characteristic)
+        if characteristic not in party_characteristics_df.columns:
+            return Response(status=204)
+    # add year feature
+    party_characteristics_df.loc[:, 'year'] = pd.to_datetime(party_characteristics_df['date']).dt.year
+    party_characteristics_df_long = party_characteristics_df.melt(
+        id_vars=['date', 'year', 'trail'],
+        value_vars=[characteristic],
+        var_name='characteristic',
+        value_name='value'
+    )
+    party_characteristics_df_long = party_characteristics_df_long.dropna(subset=['date', 'year', 'trail', 'value'])
+    if party_characteristics_df_long.empty:
+        return Response(status=204)
+    response_data = party_characteristics_df_long.to_json(orient='records')
+    return Response(response_data, status = 200, mimetype='application/json')
 
-
-def _add_people_per_vehics_feature(df):
-    df.loc[:, 'PeoplePerVehics'] = df['PartyPeople'] / df['PartyVehics'].replace(0, np.nan)
-    return df
-
-
-def _add_year_feature(df):
-    df.loc[:, 'date'] = pd.to_datetime(df['date'])
-    df.loc[:, 'year'] = df['date'].dt.year
-    df.loc[:, 'date'] = df['date'].dt.strftime('%Y-%m-%d')
-    return df
-
-
-def _calculate_yearly_mean_and_CI(df):
+def get_project_party_characteristics_yearly_statistics(project, characteristic):
+    project_sites = get_project_sites(project)
+    siteids = set(project_sites['siteid'].unique())
+    party_characteristics_df = get_from_data_source('PARTY_CHARACTERISTICS_DF')
+    # Add a 'trail' feature that contains the project assoicated site id.
+    # 'trail' value set to None if no site ids in 'SiteID' are not included in the list of project siteids.
+    party_characteristics_df['trail'] = party_characteristics_df['SiteID'].apply(
+        lambda lst: next((x for x in lst if x in siteids), None))
+    # keep observations with non-missing 'trail' values
+    party_characteristics_df = party_characteristics_df.dropna(subset=['trail'])
+    if party_characteristics_df.empty:
+        return Response(status=204)
+    if characteristic not in party_characteristics_df.columns:
+        party_characteristics_df = _add_additional_features(party_characteristics_df, characteristic)
+        if characteristic not in party_characteristics_df.columns:
+            return Response(status=204)
+    # add year feature
+    party_characteristics_df.loc[:, 'year'] = pd.to_datetime(party_characteristics_df['date']).dt.year
+    party_characteristics_df_long = party_characteristics_df.melt(
+        id_vars=['date', 'year', 'trail'],
+        value_vars=[characteristic],
+        var_name='characteristic',
+        value_name='value'
+    )
+    party_characteristics_df_long = party_characteristics_df_long.dropna(subset=['date', 'year', 'trail', 'value'])
+    if party_characteristics_df_long.empty:
+        return Response(status=204)    
     grouped = (
-        df.groupby(['year', 'characteristic'])
+        party_characteristics_df_long.groupby(['year', 'characteristic'])
         .agg(mean=('value', 'mean'), std=('value', 'std'), count=('value', 'count'))
         .reset_index()
     )
+    if grouped.empty:
+        return Response(status=204)
     grouped = grouped.sort_values(by='year')
     grouped['upper'] = grouped['mean'] + CONFIDENCE_LEVEL * grouped['std'] / np.sqrt(grouped['count'])
     grouped['lower'] = grouped['mean'] - CONFIDENCE_LEVEL * grouped['std'] / np.sqrt(grouped['count'])
-    response_data = {"labels": grouped['year'].unique().tolist()}
-    for characteristic in grouped['characteristic'].unique():
-        characteristic_data = grouped[grouped['characteristic'] == characteristic]
-        response_data[characteristic] = {
-            "mean": [None if math.isnan(value) else round(value, 2) for value in characteristic_data['mean']],
-            "upper_bound": [None if math.isnan(value) else round(value, 2) for value in characteristic_data['upper']],
-            "lower_bound": [None if math.isnan(value) else round(value, 2) for value in characteristic_data['lower']]
+    response_data = {
+        "years": grouped['year'].unique().tolist(),
+        characteristic: {
+            "mean": grouped.loc[grouped['characteristic'] == characteristic, 'mean'].round(2).tolist(),
+            "upper_bound": grouped.loc[grouped['characteristic'] == characteristic, 'upper'].round(2).tolist(),
+            "lower_bound": grouped.loc[grouped['characteristic'] == characteristic, 'lower'].round(2).tolist()
         }
-    return response_data
-
-
-def _generate_empty_time_series_response():
-    return {
-        "labels": [],
-        **{char: {"mean": [], "upper_bound": [], "lower_bound": []} for char in PARTY_CHARACTERISTICS}
     }
+    return jsonify(response_data), 200
+
+
+def get_party_characteristics_yearly_statistics(siteid, characteristic):
+    party_characteristics_df = get_from_data_source('PARTY_CHARACTERISTICS_DF')
+    # Add a 'trail' feature that contains the given siteid.
+    # 'trail' value set to None if 'SiteID' does not include siteid.
+    party_characteristics_df['trail'] = party_characteristics_df['SiteID'].apply(
+        lambda x: siteid if siteid in x else None)
+    # keep observations with non-missing 'trail' values
+    party_characteristics_df = party_characteristics_df.dropna(subset=['trail'])
+    if party_characteristics_df.empty:
+        return Response(status=204)
+    if characteristic not in party_characteristics_df.columns:
+        party_characteristics_df = _add_additional_features(party_characteristics_df, characteristic)
+        if characteristic not in party_characteristics_df.columns:
+            return Response(status=204)
+    # add year feature
+    party_characteristics_df.loc[:, 'year'] = pd.to_datetime(party_characteristics_df['date']).dt.year
+    party_characteristics_df_long = party_characteristics_df.melt(
+        id_vars=['date', 'year', 'trail'],
+        value_vars=[characteristic],
+        var_name='characteristic',
+        value_name='value'
+    )
+    party_characteristics_df_long = party_characteristics_df_long.dropna(subset=['date', 'year', 'trail', 'value'])
+    if party_characteristics_df_long.empty:
+        return Response(status=204)
+    grouped = (
+        party_characteristics_df_long.groupby(['year', 'characteristic'])
+        .agg(mean=('value', 'mean'), std=('value', 'std'), count=('value', 'count'))
+        .reset_index()
+    )
+    if grouped.empty:
+        return Response(status=204)
+    grouped = grouped.sort_values(by='year')
+    grouped['upper'] = grouped['mean'] + CONFIDENCE_LEVEL * grouped['std'] / np.sqrt(grouped['count'])
+    grouped['lower'] = grouped['mean'] - CONFIDENCE_LEVEL * grouped['std'] / np.sqrt(grouped['count'])
+    response_data = {
+        "years": grouped['year'].unique().tolist(),
+        characteristic: {
+            "mean": grouped.loc[grouped['characteristic'] == characteristic, 'mean'].round(2).tolist(),
+            "upper_bound": grouped.loc[grouped['characteristic'] == characteristic, 'upper'].round(2).tolist(),
+            "lower_bound": grouped.loc[grouped['characteristic'] == characteristic, 'lower'].round(2).tolist()
+        }
+    }
+    return jsonify(response_data), 200
+
+
+def _add_additional_features(df, feature):
+    if feature == 'PeoplePerVehics':
+        if 'PartyPeople' in df.columns and 'PartyVehics' in df.columns:
+            df.loc[:, 'PeoplePerVehics'] = df['PartyPeople'] / df['PartyVehics'].replace(0, np.nan)
+    return df
+
+
+
+
+
+# def _generate_empty_time_series_response():
+#     return {
+#         "labels": [],
+#         **{char: {"mean": [], "upper_bound": [], "lower_bound": []} for char in PARTY_CHARACTERISTICS}
+#     }
