75e72512528e59e01bdf05994be22e5a3f7f6c8e
davidye007 <davidyeap@outlook.com>
2025-01-12 00:23:10 -0800

    enable hash siteid search (dropdown won't show anything, but polygon highlight and pressing 'enter' on #{siteid} will work)

--- trails-viz-app/src/components/TopBar.vue
+++ trails-viz-app/src/components/TopBar.vue
@@ -10,96 +10,132 @@
       </b-navbar-nav>
       <b-navbar-nav class="ml-auto">
         <b-nav-form v-on:submit="doNothing">
-          <b-form-input class="form-input" size="sm" list="project-sites-list" placeholder="Search Trail"
-                        v-model="siteSearchText" v-on:keyup="autoCompleteSite" v-on:change="emitSiteNameEvent"
-                        v-show="this.$store.getters.getSelectedProjectCode"></b-form-input>
-          <b-form-datalist id="project-sites-list" :options="filteredSites"></b-form-datalist>
+          <b-form-input
+            class="form-input"
+            size="sm"
+            list="project-sites-list"
+            placeholder="Search Trail"
+            v-model="siteSearchText"
+            v-on:keyup="autoCompleteSite"
+            v-on:change="emitSiteNameEvent"
+            v-show="this.$store.getters.getSelectedProjectCode"
+          ></b-form-input>
+          <b-form-datalist
+            id="project-sites-list"
+            :options="filteredSites"
+          ></b-form-datalist>
 
-          <b-button size="sm" class="my-2 my-sm-0" variant="info" to="/login"
-                    v-show="this.$store.getters.getLoggedInUser === 'anon'">Login</b-button>
+          <b-button
+            size="sm"
+            class="my-2 my-sm-0"
+            variant="info"
+            to="/login"
+            v-show="this.$store.getters.getLoggedInUser === 'anon'"
+            >Login</b-button
+          >
 
-          <b-nav-item-dropdown v-bind:text="this.$store.getters.getLoggedInUser"
-                               v-show="this.$store.getters.getLoggedInUser !== 'anon'" right>
-            <b-dropdown-item v-on:click="gotoUserProfile">Profile</b-dropdown-item>
-            <b-dropdown-item v-if="this.$store.getters.getUserRole === 'admin'"
-                             v-on:click="gotoAdministration">Administration</b-dropdown-item>
+          <b-nav-item-dropdown
+            v-bind:text="this.$store.getters.getLoggedInUser"
+            v-show="this.$store.getters.getLoggedInUser !== 'anon'"
+            right
+          >
+            <b-dropdown-item v-on:click="gotoUserProfile"
+              >Profile</b-dropdown-item
+            >
+            <b-dropdown-item
+              v-if="this.$store.getters.getUserRole === 'admin'"
+              v-on:click="gotoAdministration"
+              >Administration</b-dropdown-item
+            >
 
             <b-dropdown-item v-on:click="logout">Logout</b-dropdown-item>
           </b-nav-item-dropdown>
-
         </b-nav-form>
       </b-navbar-nav>
     </b-collapse>
-
   </b-navbar>
 </template>
 
 <script>
+import { EventBus } from "../event-bus";
+import { Cookie } from "../cookie";
 
-  import {EventBus} from '../event-bus'
-  import {Cookie} from "../cookie";
-
-  export default {
-    name: "TopBar",
-    data: function () {
-      return {
-        siteSearchText: '',
-        filteredSites: []
-      };
-    },
-    methods: {
-      autoCompleteSite:  function() {
-        // let trailNames = Object.keys(this.$store.getters.getProjectSites);
-        let trailNames = Object.values(this.$store.getters.getProjectSites).map(site => site.trailName);
-
-        if (trailNames.includes(this.siteSearchText)) {
-          return
-        }
-        if (this.siteSearchText.length >= 2) {
-          this.filteredSites = trailNames.filter(name => name.toUpperCase().includes(this.siteSearchText.toUpperCase()));
-        } else {
-          this.filteredSites = []
-        }
-        EventBus.$emit('top-bar:site-search-results', this.filteredSites)
-      },
-      emitSiteNameEvent: function() {
-        const projectSites = Object.values(this.$store.getters.getProjectSites);
-        const selectedSite = projectSites.find(site => site.trailName === this.siteSearchText);
-        if (selectedSite) {
-          EventBus.$emit('top-bar:site-selected', selectedSite); // Emit the site object
-        } else {
-          console.warn('Site not found:', this.siteSearchText);
-        }
-        EventBus.$emit('top-bar:site-search-results', [])
-        this.filteredSites = []
-        this.siteSearchText = '';
-      },
-      doNothing: function(event) {
-        event.preventDefault()
-      },
-      gotoUserProfile: function () {
-        let username = this.$store.getters.getLoggedInUser;
-        this.$router.push({name: 'user', params: {username: username}});
-      },
-      gotoAdministration: function () {
-        this.$router.push({path: '/administration'});
-      },
-      logout: function () {
-        this.$store.dispatch('setLoggedInUser', 'anon');
-        Cookie.delete('username');
-        Cookie.delete('authHeader');
-        Cookie.delete('userRole');
-
-        if (this.$route.path !== '/') {
-          this.$router.push({path: '/'})
-        }
+export default {
+  name: "TopBar",
+  data: function() {
+    return {
+      siteSearchText: "",
+      filteredSites: [],
+    };
+  },
+  methods: {
+    autoCompleteSite: function() {
+      const projectSites = Object.values(this.$store.getters.getProjectSites);
+      const trailNames = projectSites.map((site) => site.trailName);
+      if (
+        this.siteSearchText.startsWith("#") &&
+        this.siteSearchText.length >= 2
+      ) {
+        const siteIdSearchText = this.siteSearchText.substring(1);
+        this.filteredSites = projectSites
+          .filter((site) => site.siteid.includes(siteIdSearchText))
+          .map((site) => site.trailName);
+      } else if (this.siteSearchText.length >= 2) {
+        this.filteredSites = trailNames.filter((name) =>
+          name.toUpperCase().includes(this.siteSearchText.toUpperCase())
+        );
+      } else {
+        this.filteredSites = [];
       }
-    }
-  }
+      EventBus.$emit("top-bar:site-search-results", this.filteredSites);
+    },
+    emitSiteNameEvent: function() {
+      const projectSites = Object.values(this.$store.getters.getProjectSites);
+      let selectedSite = undefined;
+      if (this.siteSearchText.startsWith("#")) {
+        selectedSite = projectSites.find(
+          (site) => site.siteid === this.siteSearchText.substring(1)
+        );
+      } else {
+        selectedSite = projectSites.find(
+          (site) => site.trailName === this.siteSearchText
+        );
+      }
+      if (selectedSite) {
+        EventBus.$emit("top-bar:site-selected", selectedSite); // Emit the site object
+      } else {
+        console.warn("Site not found:", this.siteSearchText);
+      }
+      EventBus.$emit("top-bar:site-search-results", []);
+      this.filteredSites = [];
+      this.siteSearchText = "";
+    },
+    doNothing: function(event) {
+      event.preventDefault();
+    },
+    gotoUserProfile: function() {
+      let username = this.$store.getters.getLoggedInUser;
+      this.$router.push({ name: "user", params: { username: username } });
+    },
+    gotoAdministration: function() {
+      this.$router.push({ path: "/administration" });
+    },
+    logout: function() {
+      this.$store.dispatch("setLoggedInUser", "anon");
+      Cookie.delete("username");
+      Cookie.delete("authHeader");
+      Cookie.delete("userRole");
+
+      if (this.$route.path !== "/") {
+        this.$router.push({ path: "/" });
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .form-input {
-    margin: 0 10px 0 10px;
-  }
+.form-input {
+  margin: 0 10px 0 10px;
+}
 </style>
